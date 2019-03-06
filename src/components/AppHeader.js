b49ce1b638ecf906bae6810043fa5206dfa023a5
James Chan <jwchan1993@gmail.com>
2019-03-06 23:49:54 +0000

    Adder hyperlink to donate icon

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Header} from 'react-native-elements';
+import {Linking} from 'react-native';
 
 export default class AppHeader extends Component{
     render(){
@@ -8,7 +9,7 @@ export default class AppHeader extends Component{
                 backgroundColor='#1E234C'
                 containerStyle={{ height: 50 }}
                 centerComponent={{ text: '10xGREENER', style: { color: '#72E1A4', paddingBottom: 20 } }}
-                rightComponent={{ text: '$', style: {color: '#F0AD3D', paddingBottom: 20, fontWeight: 'bold', fontSize: 20 } }}
+                rightComponent={{ text: '$', onPress: () => {Linking.openURL('https://act.friendsoftheearth.uk/donate/make-donation')},style: {color: '#F0AD3D', paddingBottom: 20, fontWeight: 'bold', fontSize: 20 } }}
             />
         )
     }
